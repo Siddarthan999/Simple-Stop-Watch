91bf095d527e62911857f91d776af9ddde315c02
Siddarthan999 <siddarthan999@gmail.comm>
2024-07-02 13:29:01 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { TimerContextProvider } from './components/TimerContext.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TimerContextProvider>
       <App />
+      <Analytics />
     </TimerContextProvider>
   </React.StrictMode>,
 )
